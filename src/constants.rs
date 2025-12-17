1a6f785ae8f5ee99fa0a583174048e168aea70d5
ssfortynine <ssfortynine@gmail.com>
2025-12-17 22:12:27 +0800

    Correct private constant `MAX_SAMPLES` error

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,3 @@
 pub const TICK_RATE_MS: u64 = 500; //
 pub const HISTORY_WINDOW_SECS: u64 = 60;  
-const MAX_SAMPLES: usize = (HISTORY_WINDOW_SECS * 1000 / TICK_RATE_MS) as usize;
+pub const MAX_SAMPLES: usize = (HISTORY_WINDOW_SECS * 1000 / TICK_RATE_MS) as usize;
